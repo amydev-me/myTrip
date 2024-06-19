ac2a3841614262403665443faf255514c2920c61
amypyae <ampyaephyonaing@gmail.com>
2024-06-19 17:03:46 +0800

    User Registration

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -1,50 +1,54 @@
 import express, { Request, Response } from 'express';
 import { User } from "../models/user";
 import jwt from 'jsonwebtoken';
+import { check, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.get('/test', (req: Request, res: Response) => {
-    res.status(200).json({
-        message: "Hello World!"
-    })
-});
+router.post("/register", [
+        check("firstName", "First Name is required").isString(),
+        check("lastName", "Last Name is required").isString(),
+        check("email", "Email is required").isEmail(),
+        check("password", "Password with 6 or more characters required").isLength({ min: 6 })
+    ], async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ message: errors.array() });
+        }
+        const { email, password, firstName, lastName } = req.body;
+        try {
+            const existingUser = await User.findOne({ email });
 
+            if(existingUser) {
+                return res.status(400).json({
+                    message: "User already exists"
+                });
+            }
 
-router.post("/register", async (req: Request, res: Response) => {
-    const { email, password, firstName, lastName } = req.body;
-    try {
-        const existingUser = await User.findOne({ email });
+            const user = User.build({
+                email, password, firstName, lastName
+            });
 
-        if(existingUser) {
-            return res.status(400).json({
-                message: "User already exists"
+            await user.save();
+
+            const userJwt = jwt.sign({
+                id: user.id,
+                email: user.email
+            }, process.env.JWT_KEY!);
+
+            req.session = {
+                jwt : userJwt
+            };
+
+            return res.status(200).send({ message: "User registered OK" });
+
+        }catch(error) {
+            console.log(error);
+            return res.status(500).json({
+                message: "Something went wrong."
             });
         }
-
-        const user = User.build({
-            email, password, firstName, lastName
-        });
-
-        await user.save();
-
-        const userJwt = jwt.sign({
-            id: user.id,
-            email: user.email
-        }, process.env.JWT_KEY!);
-
-        req.session = {
-            jwt : userJwt
-        };
-
-        return res.status(200).send({ message: "User registered OK" });
-
-    }catch(error) {
-        console.log(error);
-        return res.status(500).json({
-            message: "Something went wrong."
-        });
-    }
 });
+
 
 export { router as userRoutes };
